ee0f1234cbef5346533971d49b7f05681d192797
x1lefg0r <ebalynin@mail.ru>
2026-03-08 02:08:54 +0300

    restyle: made a card that is close to completing the task

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,36 +1,35 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useProductsFilter } from "@/hooks/useProductsFilter";
 import type { ProductProps } from "@/types";
 
 export const ProductCard = ({ product }: ProductProps) => {
-  const { title, price, image, description } = product;
+  const { setProduct } = useProductsFilter();
+  const { title, price, image } = product;
 
   return (
-    <Card className="relative mx-auto w-full max-w-sm pt-0">
+    <Card
+      className="relative mx-auto w-full max-w-sm pt-0 cursor-pointer transition-transform duration-300 hover:scale-105"
+      onClick={() => setProduct(String(product.id))}
+    >
       <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
       <img
         src={image}
-        alt="Event cover"
+        alt={title}
         className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
       />
       <CardHeader>
-        <CardAction>
-          <Badge variant="secondary">{price}</Badge>
-        </CardAction>
+        <CardDescription>
+          <span className="font-semibold">
+            {price.toLocaleString("ru-RU")} ₽
+          </span>
+        </CardDescription>
         <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
       </CardHeader>
-      <CardFooter>
-        <Button className="w-full">Смотреть больше</Button>
-      </CardFooter>
     </Card>
   );
 };
