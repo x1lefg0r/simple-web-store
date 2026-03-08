5709068ca710aa8a405bcf61c3b1c909d16e9fad
x1lefg0r <ebalynin@mail.ru>
2026-03-09 02:36:04 +0300

    fix: bug wth search input

--- src/components/filters/SearchFilter.tsx
+++ src/components/filters/SearchFilter.tsx
@@ -1,12 +1,15 @@
 import type { SearchFilterProps } from "@/types";
 import { Input } from "../ui/input";
+import { useProductsFilter } from "@/hooks/useProductsFilter";
 
 export const SearchFilter = ({ onChange }: SearchFilterProps) => {
+  const { search } = useProductsFilter();
   return (
     <div className="sticky top-5 z-40 max-w-md mx-auto bg-white rounded-md">
       <Input
         type="search"
         placeholder="Поиск..."
+        defaultValue={search}
         onChange={(e) => onChange(e.target.value)}
       />
     </div>
