34058766efc24a6a12fb36e84f78a1152ef62cc0
x1lefg0r <ebalynin@mail.ru>
2026-03-11 01:09:56 +0300

    chore: added vitest library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
@@ -17,5 +17,9 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
