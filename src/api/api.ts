5ad73ab61f8caa69b96027f5b778023b0b704d81
x1lefg0r <ebalynin@mail.ru>
2026-03-11 01:29:19 +0300

    chore: added env for api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,12 @@
 import type { Product, ProductsResponse } from "../types";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const fetchProducts = async (
   search: string,
   page: number,
 ): Promise<ProductsResponse> => {
-  const url = new URL("http://localhost:3000/products");
+  const url = new URL(`${API_URL}/products`);
   url.searchParams.set("_page", String(page));
   url.searchParams.set("_per_page", "20");
   if (search) url.searchParams.set("title:contains", search);
@@ -19,7 +21,7 @@ export const fetchProducts = async (
 };
 
 export const fetchProduct = async (id: number): Promise<Product> => {
-  const response = await fetch(`http://localhost:3000/products/${id}`);
+  const response = await fetch(`${API_URL}/products/${id}`);
 
   if (!response.ok) {
     throw new Error(`HTTP error: ${response.status}`);
